7d4eb8d264cd494defdea4d32cf0d1738acc663f
sejan <sejan157218@gmail.com>
2021-09-23 17:21:08 +0600

    add data in localStorage by object

--- src/Componet/AddID/AddId.js
+++ src/Componet/AddID/AddId.js
@@ -1,13 +1,21 @@
 
 const AddID = id => {
-   const exist = localStorage.getItem(id);
-   if(!exist){
-    localStorage.setItem(id,1);
+   const exists = localStorage.getItem('shopping_cart');
+   let shopping_cart = {}
+   if(!exists){
+    shopping_cart[id] = 1;
    }
    else{
-       const newValue = parseInt(exist) +1;
-       localStorage.setItem(id,newValue);
+    shopping_cart = JSON.parse(exists);
+       if(shopping_cart[id]){
+           const newValue = shopping_cart[id] +1;
+           shopping_cart[id] = newValue;
+       }
+       else{
+        shopping_cart[id] = 1;
+       }
    }
+   localStorage.setItem('shopping_cart',JSON.stringify(shopping_cart))
 };
 
 export default AddID;
